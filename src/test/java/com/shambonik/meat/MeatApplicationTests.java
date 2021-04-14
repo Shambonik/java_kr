8cd0bd62f2e03b32fa6ded947d75ee1282d9c842
Shambonik <shambonik@gmail.com>
2021-04-15 02:22:07 +0300

    Project created

---
+++ src/test/java/com/shambonik/meat/MeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shambonik.meat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
